fc94597ac1703526dfaed6aff2266d55a73601a2
Cameron Pam <cammpam.creative@gmail.com>
2017-09-13 05:07:03 -0700

    Restart  Options added

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,16 +8,45 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            string input = Console.ReadLine();
-            string output = "";
+        static void Main()
+        {          
+            string input, output = "";
+            Console.WriteLine("Whats your Word? ");
+            input = Console.ReadLine();
+
             for (int i = input.Length - 1; i >= 0; i--)
             {
                 output += input[i];
             }
-            Console.WriteLine(output);
+            Console.WriteLine("Your word Reverse: {0}", output);
+            Restart();
             Console.ReadLine();
+
         }
+        static void Restart()
+        {
+            Console.WriteLine("Restart Y/N");
+            string input = Console.ReadLine();
+            if (input == "Y" || input == "y")
+            {
+                Main();
+            }
+            if (input == "N" || input == "n")
+            {
+                Console.WriteLine("Thank You. Come Again!");
+                Environment.Exit(0);
+
+            }
+            else
+            {
+                Console.WriteLine("Enter Y/y or N/n. Thank you.");
+                    Restart();
+            }
+
+
+            
+
+        }
+         
     }
 }
