b0295a981cff246b4f9f12c0f0a24f5b26608112
Cameron Pam <cammpam.creative@gmail.com>
2017-09-13 18:16:23 -0700

    Cat Garden started

---
+++ Cat Garden/Josies Cat Garden/Josies Cat Garden/Josies Cat Garden/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Josies_Cat_Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        Console.WriteLine("Catnip");
+                    }
+                else
+                {
+                        Console.WriteLine("Mint ");
+                    }
+                }
+                Console.WriteLine();
+                Console.Read();
+            }
+        }
+    }
+}
