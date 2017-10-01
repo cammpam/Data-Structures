e6aab17b41177ff454f322aa7691bb22011c8095
Cameron Pam <cammpam.creative@gmail.com>
2017-10-01 13:26:37 -0700

    Code Challenge Implement a stack with 2 queues

---
+++ StackTwoQueues/StackTwoQueues/StackTwoQueues/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackTwoQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queueTest = new Queue(1);
+            queueTest.Enqueue(2);
+            queueTest.Enqueue(3);
+            queueTest.StkOne.View();
+            Console.WriteLine("Add More\n");
+            queueTest.Enqueue(4);
+            queueTest.Enqueue(5);
+            queueTest.StkOne.View();
+
+            Console.WriteLine();
+
+            Console.WriteLine($"{ queueTest.Dequeue().Data} was removed!");
+            queueTest.StkOne.View();
+            Console.Read();
+        }
+    }
+}
