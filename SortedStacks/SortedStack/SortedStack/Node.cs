3ed0bd5d5d374820830595372f5824efc0f942bc
Cameron Pam <cammpam.creative@gmail.com>
2017-10-01 13:53:45 -0700

    Code Challenge SortedStack

---
+++ SortedStacks/SortedStack/SortedStack/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedStack
+{
+    class Node
+    {
+        public Node Next { get; set; }
+        public Node Prev { get; set; }
+        public object Data { get; set; }
+    }
+}
---
+++ SortedStacks/SortedStack/SortedStack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack:");
+            Stack testS = new Stack();
+            testS.Push("1");
+            testS.Push("2");
+            testS.Push("3");
+            testS.Push("4");
+            testS.Push("5");
+            testS.Push("6");
+            testS.Push("7");
+            testS.Push("8");
+            testS.Push("9");
+   
+
+            testS.SortPush("");
+
+            Console.Read();
+        }
+    }
+}
