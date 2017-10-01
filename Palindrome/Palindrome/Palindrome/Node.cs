fbd62518ffd07b3c598dab3de2bce89d40d36efc
Cameron Pam <cammpam.creative@gmail.com>
2017-10-01 12:56:41 -0700

    Code Challenge Palindrome

---
+++ Palindrome/Palindrome/Palindrome/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Node
+    {
+        public string Data { get; set; }
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
+
+        public Node(string data)
+        {
+            Data = data;
+            Next = null;
+            Previous = null;
+        }
+    }
+}
---
+++ Palindrome/Palindrome/Palindrome/Queue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Queue : LinkedList
+    {
+        public Queue(string data)
+        {
+            Head = new Node(data);
+            Tail = Head;
+        }
+
+        public void Enqueue(string data)
+        {
+            Node temp = Tail;
+            Tail.Next = new Node(data);
+            Tail = Tail.Next;
+            Tail.Previous = temp;
+        }
+
+        public string Dequeue()
+        {
+            if (Head == Tail) { Tail = null; }
+            Node temp = Head.Next;
+            Head.Next = null;
+            string value = Head.Data;
+            Head = temp;
+            return value;
+        }
+
+        public string IsPalindrome()
+        {
+            Node start = Head;
+            Node end = Tail;
+
+            while (start.Next != null && end.Previous != null)
+            {
+                if (start.Data.Equals(end.Data) == true)
+                {
+                    start = start.Next;
+                    end = end.Previous;
+                }
+                else
+                {
+                    return "Nope This is not a Palindrome!";
+                }
+            }
+            return "YAYYY Palindrome!!!";
+        }
+    }
+}
