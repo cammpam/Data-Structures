bf6ae8359744622c7a7d10d19aef544f73e3feb7
Cameron Pam <cammpam.creative@gmail.com>
2017-10-15 18:01:33 -0700

    BreadthOrder

---
+++ BinaryTrees/BreathOrder/BTBreathOrder/BTBreathOrder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTBreathOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //root node
+            BinaryTree newTree = new BinaryTree();
+
+            // collects nodes 
+            newTree.Root = new Node(4);
+            newTree.Root.Left = new Node(2);
+            newTree.Root.Right = new Node(6);
+            newTree.Root.Left.Left = new Node(1);
+            newTree.Root.Left.Right = new Node(3);
+            newTree.Root.Right.Left = new Node(5);
+            newTree.Root.Right.Right = new Node(7);
+
+            // BreathOrder
+            Console.Write("Breadth Order: ");
+            newTree.BreadthOrder(newTree.Root);
+            Console.Read();
+        }
+    }
+}
---
+++ BinaryTrees/BreathOrder/BTBreathOrder/BTBreathOrder/binaryTree.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTBreathOrder
+{
+    class BinaryTree
+    {
+        public Node Root;
+
+        // initializes tree
+        public BinaryTree() => Root = null;
+
+        public void BreadthOrder(Node Current)
+        {
+            Queue<Node> tree = new Queue<Node>();
+
+            if (Current == null) { return; }
+
+            tree.Enqueue(Current);
+
+            while (tree.Count > 0)
+            {
+                Node Counter = tree.Dequeue();
+                if (Counter.Left != null)
+                {
+                    tree.Enqueue(Counter.Left);
+                }
+                if (Counter.Right != null)
+                {
+                    tree.Enqueue(Counter.Right);
+                }
+                Console.Write($"{Counter.Value} ");
+            }
+        }
+
+        public void PreOrder(Node Current)
+        {
+            if (Current != null)
+            {
+                Console.Write($"{Current.Value} ");
+                PreOrder(Current.Left);
+                PreOrder(Current.Right);
+            }
+        }
+        public void PostOrder(Node Current)
+        {
+            if (Current != null)
+            {
+                PostOrder(Current.Left);
+                PostOrder(Current.Right);
+                Console.Write($"{Current.Value} ");
+            }
+        }
+        public void InOrder(Node Current)
+        {
+            if (Current != null)
+            {
+                InOrder(Current.Left);
+                Console.Write($"{Current.Value} ");
+                InOrder(Current.Right);
+            }
+        }
+    }
+    public class Node
+    {
+        public object Value;
+        public Node Left;
+        public Node Right;
+
+        public Node(object leafValue)
+        {
+            Value = leafValue;
+            Left = null;
+            Right = null;
+        }
+    }
+}
