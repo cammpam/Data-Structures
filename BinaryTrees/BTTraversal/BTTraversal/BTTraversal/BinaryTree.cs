d534be49d61e63d71136950f1f2a0ead85015387
Cameron Pam <cammpam.creative@gmail.com>
2017-10-16 21:35:51 -0700

    Binary Search Trees

---
+++ BinaryTrees/BTTraversal/BTTraversal/BTTraversal/BinaryTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTTraversal
+{
+    class BinaryTree
+    {
+        //Create Root Node
+        public Node Root;
+
+        //Initializes new binary tree with Root Node
+        public BinaryTree(int newNode) => Root = new Node(newNode);
+
+        public void Add(Node current, int value)
+        {
+            //if value is less then current node value, move to next if statment
+            if (value < current.Value)
+            {
+                //if the left current node is null create new node with the compared value
+                if (current.Left == null)
+                {
+                    current.Left = new Node(value);
+                    return;
+                }
+                else
+                {
+                    //Repeat Add() from current.Left
+                    Add(current.Left, value);
+                    return;
+                }
+            }
+            //Add() if value is GREATER then current
+            if (value > current.Value)
+            {
+                if (current.Right == null)
+                {
+                    current.Right = new Node(value);
+                    return;
+                }
+                else
+                {
+                    Add(current.Right, value);
+                    return;
+                }
+            }
+        }
+
+
+
+
+    }
+}
---
+++ BinaryTrees/BTTraversal/BTTraversal/BTTraversal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree newTree = new BinaryTree(12);
+
+
+            newTree.Add(newTree.Root, 1);
+            newTree.Add(newTree.Root, 13);
+            newTree.Add(newTree.Root, 11);
+            newTree.Add(newTree.Root, 7);
+            newTree.Add(newTree.Root, 3);
+            newTree.Add(newTree.Root, 17);
+            newTree.Add(newTree.Root, 15);
+
+            Console.Read();
+
+
+        }
+    }
+}
