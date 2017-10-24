d065651ba67b15b358c1102a34923fdd692319bc
Cameron Pam <cammpam.creative@gmail.com>
2017-10-23 22:00:55 -0700

    InsertionSort

---
+++ InsertionSort/InsertonSort/InsertonSort/ArrayAction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertonSort
+{
+    class ArrayAction
+    {
+        //    int[] array = new int[7] { 8, 16, 13, 12, 5, 3, 22 };
+
+
+        //    public static void SortArray(int[] array)
+        //    {
+        //        for (var i = 0; i < array.Length; i++)
+        //        {
+        //            for(var j = 0; j < array.Length; i++)
+        //            {
+        //                if(array[i] < array[j])
+        //                {
+        //                    array[j - 1] = j;
+        //                    array[i] = array[j];
+        //                }
+        //            }
+        //        }
+        //    }
+
+
+    //    public static void PrintArray(int[] array)
+    //{
+    //    foreach (int i in array)
+    //    {
+    //        Console.WriteLine(i);
+    //    }
+    //}
+    }
+}
