bc4cad14c88e4a6fc8a8704abd89cca85a85c178
Cameron Pam <cammpam.creative@gmail.com>
2017-10-31 21:19:33 -0700

    AssocArray

---
+++ AssociativeArrary/AssociativeArray/AssociativeArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssociativeArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AssocArray newAA = new AssocArray();
+            newAA.Add("Monkey");
+            newAA.Add("Rabbit");
+            Console.WriteLine($"{newAA.Search("Monkey")}");
+            Console.WriteLine($"{newAA.Delete("Rabbit")}");
+            Console.Read();
+        }
+    }
+}
