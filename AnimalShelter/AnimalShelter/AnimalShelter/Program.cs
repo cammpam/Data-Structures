90c1621de1f4d023e1f47ae77396d677547c2d26
Cameron Pam <cammpam.creative@gmail.com>
2017-11-07 23:03:56 -0800

    Commits added

--- AnimalShelter/AnimalShelter/AnimalShelter/Program.cs
+++ AnimalShelter/AnimalShelter/AnimalShelter/Program.cs
@@ -10,11 +10,13 @@ namespace AnimalShelter
     {
         public class Animal
         {
+            //Set properties for each animal
             public string AnimalType { get; set; }
             public string AnimalName { get; set; }
 
             public Animal() { }
 
+            //Animal Constructor
             public Animal(string type, string name)
             {
                 AnimalType = type;
@@ -23,6 +25,7 @@ namespace AnimalShelter
         }
         static void Main(string[] args)
         {
+            //Add to Queue and print adopted animal by name
             Console.WriteLine("Animal Shelter");
             Console.WriteLine();
             Queue<Animal> shelter = new Queue<Animal>();
@@ -40,17 +43,20 @@ namespace AnimalShelter
 
         public static Animal Adopt(Queue<Animal> shelter)
         {
+            //Remove adopted animal from queue 
             return shelter.Dequeue();
         }
 
         public static Animal Adopt(Queue<Animal> shelter, string species)
         {
+            //removes the last animal if the species == the animaltype
             if (species == shelter.Peek().AnimalType)
             {
                 return shelter.Dequeue();
             }
             else
             {
+                //else create a new instance of animal 
                 Animal newAnimal = new Animal();
                 Animal temp = shelter.Dequeue();
                 string name = temp.AnimalName;
