fc583be2fab406e81e96a2f4ee56b4a71a5b887c
Cameron Pam <cammpam.creative@gmail.com>
2017-11-07 23:33:26 -0800

    Tower of Hanoi Cameron

--- TowerOfHanoi/TowerOfHanoi/TowerOfHanoi/Program.cs
+++ TowerOfHanoi/TowerOfHanoi/TowerOfHanoi/Program.cs
@@ -10,6 +10,40 @@ namespace TowerOfHanoi
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number of disks:");
+            Console.WriteLine();
+            int values = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"For {values} disks, make the following moves:");
+            Tower(values, 1, 3);
+            int moves = Convert.ToInt32(Math.Pow(2, values) - 1);
+            Console.WriteLine();
+            Console.WriteLine($"This took {moves} moves.");
+            Console.Read();
+        }
+
+        // moves q and their values, one at a time, from one stack to another stack
+        public static void Tower(int q, int StackNext, int StackPrev)
+        {
+            int holdingStack;
+
+            if (q == 1)
+            {
+                Console.WriteLine($"{StackNext} -> {StackPrev}");
+            }
+            else
+            {
+
+                holdingStack = 6 - (StackNext + StackPrev);
+
+                // Move q values to holding stack execpt largest value 
+                Tower(q - 1, StackNext, holdingStack);
+
+                // Move largest value to destination stack
+                Console.WriteLine($"{StackNext} -> {StackPrev}");
+
+                // Move disks from helpStack to destination
+                Tower(q - 1, holdingStack, StackPrev);
+            }
         }
     }
 }
